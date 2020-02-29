2d3f58c9f5f6fcab6c7d9d15ebc3d2dc1b9accb8
Adeshina Adekunle <jc483768@ad.jcu.edu.au>
2020-03-01 08:15:34 +1000

    model set up

---
+++ Kendrick_model.R
@@ -0,0 +1,5 @@
+#The Kendrick Taylor model is an economic growth model. It is establishes 
+#the relationship between consumption and captial
+require(MASS)
+final_time = seq(from =20,to =100, by=10)
+len_final_time = length(final_time)
